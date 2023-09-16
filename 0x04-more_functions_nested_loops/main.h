a045df17ff7842bc450f59ca231476adb03aad86
beza Alex <bezubeza32@gmail.com>
2023-09-16 14:14:11 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_numbers(void);
+int _putchar(int i);
 int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
-int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
