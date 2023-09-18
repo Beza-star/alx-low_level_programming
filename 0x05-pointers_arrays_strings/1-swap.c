308812c4a1723b868e38d52cd51e6cb0c049ce91
beza Alex <bezubeza32@gmail.com>
2023-09-18 14:10:39 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  swap_int - swap the value of 'a' with 'b'
+ *  @a: type int argument
+ *  @b: type int  argument
+ *  return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
