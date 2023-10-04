02b971a11daf666bfa893a9a0c2ba06250402114
beza Alex <bezubeza32@gmail.com>
2023-10-04 14:13:23 -0700

    4-free_grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * free_grid - free a 2-dimensional array created by alloc_grid
  * @grid: a pointer to the first column of the first row of the array
