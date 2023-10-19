41c516f681d335af547cda2364a26aa1f1b5c1a4
beza Alex <bezubeza32@gmail.com>
2023-10-19 14:16:16 -0700

    list the list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include "lists.h"
-
+#include <stdio.h>
 /**
  * list_len - function with one argument
- * @h: const list_t pointer argument to struct
- *
- * Description: returns the number of elements in a linked list
- * Return: number of elements
+ * @h: const list_t pointer argument to stru
+ * Return: 1
  */
 size_t list_len(const list_t *h)
 {
 	unsigned int count = 0;
-
-	while (h != NULL)
+       
+if (h !=0)
 	{
 		h = h->next;
 		count++;
