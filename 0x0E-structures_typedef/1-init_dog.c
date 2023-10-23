b6937c431a3ce8649b527f3746b55739f3b0a237
beza Alex <bezubeza32@gmail.com>
2023-10-23 05:44:30 -0700

    dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,12 +8,14 @@
  * @owner: char type argument
  * Return: 0
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+  if (d == NULL)
+    ;
+  else
+    {
+      d->name = name;
+      d->age = age;
+      d->owner = owner;
+    }
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,63 +7,70 @@
  * @owner: char type pointer
  * Return: 0
  */
-dog_t *new_dog(char *name, float age, char *owner)
-char *_strdup(char *str);
+int _strlen(char *s)
 {
-	dog_t *new_d;
+	int count = 0;
 
-	new_d = malloc(sizeof(dog_t));
-	if (new_d == NULL)
-		return (NULL);
-
-	new_d->name = _strdup(name);
-	if (!new_d->name)
+	if (s != '\0')
 	{
-		free(new_d);
-		return (NULL);
+		while (*(s + count) != '\0')
+			count++;
 	}
-	new_d->age = age;
-	new_d->owner = _strdup(owner);
-	if (!new_d->owner)
-	{
-		free(new_d->name);
-		free(new_d);
-		return (NULL);
-	}
-
-	return (new_d);
-
+	return (count);
 }
-
 /**
- * *_strdup - function with one argument
- * @str: string argument
- * Return: 0
+ * _strcpy - copies the string with \0 to the buffer
+ * @dest: pointer to dest
+ * @src: pointer to string
+ *
+ * Return: the pointer to dest
+ *
  */
-char *_strdup(char *str)
+char *_strcpy(char *dest, char *src)
 {
-	int i, j;
-	char *ptr;
+	int i;
 
-	if (str == NULL)
-		return (NULL);
 	i = 0;
-	while (*(str + i) != '\0')
+	while (*(src + i) != '\0')
 	{
+		*(dest + i) = *(src + i);
 		i++;
 	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: pointer to new struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int len_name, len_owner;
+	struct dog *new_dog;
 
-	ptr = malloc(sizeof(char) * i + 1);
-
-	if (ptr == NULL)
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
 		return (NULL);
-
-	j = 0;
-	while (str[j] != '\0')
+	len_name = _strlen(name);
+	new_dog->name = malloc(sizeof(char) * (len_name + 1));
+	if (new_dog->name == NULL)
 	{
-		ptr[j] = str[j];
-		j++;
+		free(new_dog);
+		return (NULL);
 	}
-	ptr[j] = '\0';
-	return (ptr);
+	new_dog->name = _strcpy(new_dog->name, name);
+	new_dog->age = age;
+	len_owner = _strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = _strcpy(new_dog->owner, owner);
+	return (new_dog);
 }
