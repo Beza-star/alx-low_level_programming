33c7cecf5f1eceef5a3952c1ec255774e42d1227
beza Alex <bezubeza32@gmail.com>
2023-10-24 09:17:57 -0700

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * print_listint - function to print all elements of list
  * @h: pointer to the elements of lists
@@ -8,10 +9,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != 0)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
 	return (count);
+}
