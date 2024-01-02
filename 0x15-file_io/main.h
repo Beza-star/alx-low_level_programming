76c72771acd66f3a2ee7a8a757a063983a78c2dd
root <bezubeza32@gmail.com>
2024-01-02 01:56:51 -0800

    new file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif 
